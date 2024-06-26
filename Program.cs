4eac0df06dc1a464133adda25ad7a68e81eb8a31
DenSad81 <tagil81@mail.ru>
2024-06-26 15:54:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "(()))";
+            char actualSign = ' ';
+            char signOpen = '(';
+            char signClose = ')';
+            int deep = 0;
+            int maxDeed = 0;
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                actualSign = inputString[i];
+
+                if (actualSign == signOpen)
+                    deep++;
+
+                if (actualSign == signClose)
+                    deep--;
+
+                if (deep > maxDeed)
+                    maxDeed = deep;
+            }
+
+            if ( deep != 0)
+                Console.WriteLine("Строка не верна");
+            else
+                Console.WriteLine($"Строка верна, глубина вложения {maxDeed}");
+        }
+    }
