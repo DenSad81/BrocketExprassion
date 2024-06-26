a43064abd38136936e64453aff7314e5e6c4cfe4
DenSad81 <tagil81@mail.ru>
2024-06-26 19:19:44 +0300

    correct

--- Program.cs
+++ Program.cs
@@ -4,34 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
- class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string inputString = "()(";
+        char actualSign = ' ';
+        char signOpen = '(';
+        char signClose = ')';
+        int deep = 0;
+        int maxDeed = 0;
+
+        for (int i = 0; i < inputString.Length; i++)
         {
-            string inputString = "(()))";
-            char actualSign = ' ';
-            char signOpen = '(';
-            char signClose = ')';
-            int deep = 0;
-            int maxDeed = 0;
+            actualSign = inputString[i];
 
-            for (int i = 0; i < inputString.Length; i++)
-            {
-                actualSign = inputString[i];
+            if (actualSign == signOpen)
+                deep++;
 
-                if (actualSign == signOpen)
-                    deep++;
+            if (actualSign == signClose)
+                deep--;
 
-                if (actualSign == signClose)
-                    deep--;
+            if (deep < 0)
+                break;
 
-                if (deep > maxDeed)
-                    maxDeed = deep;
-            }
-
-            if ( deep != 0)
-                Console.WriteLine("Строка не верна");
-            else
-                Console.WriteLine($"Строка верна, глубина вложения {maxDeed}");
+            if (deep > maxDeed)
+                maxDeed = deep;
         }
+
+        if (deep != 0)
+            Console.WriteLine("Строка не верна");
+        else
+            Console.WriteLine($"Строка верна, глубина вложения {maxDeed}");
     }
+}
